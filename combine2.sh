6730948133f26714cf342086da30e4f7f1b1057c
Taka <Taka@otp1232143ots.ucsf.edu>
2016-12-10 09:36:29 -0800

    First draft of combine2.sh

---
+++ combine2.sh
@@ -0,0 +1,20 @@
+#Add header to chromosome gene data
+#Last modified: Fri Dec 9. 2016
+
+echo "Starting combining script"
+for file in "$@"                                    #@ is a variable that stores all inputs to a script. So "$@" in our script specifies all the input values typed in after combine.sh
+do
+	echo "$file"
+	cat raw/header.txt "$file" > processed2/$file
+done
+
+#echo "Switching into new directory"
+#cd processed2
+
+#for input in *.txt
+#do
+#	echo "Analyzing $input ..."
+#	python Script_02.py $input
+#done
+
+echo "done!"
